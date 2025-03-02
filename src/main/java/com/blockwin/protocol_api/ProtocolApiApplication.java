4f4e0f4e0e70cc1b9995096e27b7fb8b8b0bdc33
Daniel <danidavidov13.dd@gmail.com>
2025-03-02 02:42:31 +0200

    initial commit

---
+++ src/main/java/com/blockwin/protocol_api/ProtocolApiApplication.java
@@ -0,0 +1,13 @@
+package com.blockwin.protocol_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtocolApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtocolApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blockwin/protocol_api/ProtocolApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blockwin.protocol_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtocolApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
