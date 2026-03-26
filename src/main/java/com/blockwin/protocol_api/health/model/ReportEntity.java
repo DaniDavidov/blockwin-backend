d41b3decbceb61a655fdd0a50655c17e1a83808b
Daniel <danidavidov13.dd@gmail.com>
2026-03-26 13:50:34 +0200

    PatformTick replaced with ReportEntity. The system is moving from tick based to round based

--- src/main/java/com/blockwin/protocol_api/health/model/ReportEntity.java
+++ src/main/java/com/blockwin/protocol_api/health/model/ReportEntity.java
@@ -2,6 +2,7 @@ package com.blockwin.protocol_api.health.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,9 +12,10 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Builder
 @Entity
 @Table(name = "platforms")
-public class PlatformTick {
+public class ReportEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -24,12 +26,30 @@ public class PlatformTick {
     private Status status;
 
     @Column(nullable = false)
+    private long dns;
+
+    @Column(nullable = false)
+    private long tcp;
+
+    @Column(nullable = false)
+    private long tls;
+
+    @Column(nullable = false)
+    private long ttfb;
+
+    @Column(nullable = false)
+    private long latency;
+
+    @Column(nullable = false)
     private Instant timestamp;
 
-    @Column(name = "platform_id", nullable = false)
-    private UUID platformId;
+    @Column(name = "platform_url", nullable = false)
+    private String platformUrl;
 
     @Column(name = "validator_id", nullable = false)
     private UUID validatorId;
+
+    @Column(name = "round_id", nullable = false)
+    private long roundId;
 
 }
